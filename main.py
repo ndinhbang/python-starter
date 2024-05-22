a217d57b5b96d006ae157f715574723a6e9b6e52
Nguyen Dinh Bang <bangnd@abilive.vn>
2024-05-22 16:48:40 +0700

    add collation and isolation level

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ from sqlalchemy import create_engine, URL, text
 load_dotenv()
 
 # https://docs.sqlalchemy.org/en/20/core/engines.html#creating-urls-programmatically
-con_url = URL.create(
+connection_url = URL.create(
     os.environ.get("DB_CONNECTION"),
     username=os.environ.get("DB_USERNAME"),
     password=os.environ.get("DB_PASSWORD"), # plain (unescaped) text
     host=os.environ.get("DB_HOST"),
     database=os.environ.get("DB_DATABASE"),
     port=os.environ.get("DB_PORT"),
-    query={'charset': 'utf8mb4'}
+    query={'charset': 'utf8mb4', 'collation': 'utf8mb4_unicode_ci'}
 )
-engine  = create_engine(con_url, echo=True)
+engine  = create_engine(
+    connection_url,
+    isolation_level="REPEATABLE READ", 
+    echo=True
+)
 # connect to database server 
 with engine.connect() as connection:
     #executing the query
     result = connection.execute(text("SELECT * FROM m_chat_ai_reply_context"))
-    # print(result.fetchall())
-    for row in result.mappings():
-        print("Context:" , row["context"])
+    # commit to prevent rollback log
+    connection.commit() 
+    for row in result:
+        print("Context:" , row.context)
 
 # comment this 
